13f654e71595841ab8e5a37c7f57f2a3991e1035
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2023-06-13 22:25:42 +0300

    delete 1 file and update 1 file

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -56,8 +56,8 @@ function Leaderboard() {
                                     <div className="flex items-center">
                                       <div className="flex-shrink-0 w-10 h-10">
                                           <Image
-                                              width={16}
-                                              height={12}
+                                              width={22}
+                                              height={22}
                                               src={`https://flagcdn.com/16x12/${row.countryCode}.webp`}
                                               alt={row.country}
                                           />
